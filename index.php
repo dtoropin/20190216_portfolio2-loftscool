a0761308b9abd0bbf53a98d69dffa6c58fb7a455
Denis Toropin <denis.toropin@yandex.ru>
2019-03-08 11:16:32 +0300

    modify twig template

--- index.php
+++ index.php
@@ -1,7 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 2014
- * Date: 06.03.2019
- * Time: 0:06
- */
+
+require_once 'vendor/autoload.php';
+
+$route = new \Klein\Klein();
+$templater = Templater::getInstance();
+$config = include_once 'config/config.php';
+
+$route->respond('GET', '/', function () use ($templater, $config) {
+	$data = array();
+	$data['config'] = $config;
+	$data['style'] = 'index';
+	$data['title'] = 'Главная страница | Обо мне';
+	return $templater->display('pages/index', $data);
+});
+
+$route->respond('GET', '/portfolio/?', function () use ($templater, $config) {
+	$data = array();
+	$data['config'] = $config;
+	$data['style'] = 'portfolio';
+	$data['title'] = 'Страница моих работ';
+	return $templater->display('pages/portfolio', $data);
+});
+
+$route->respond('GET', '/contacts/?', function () use ($templater, $config) {
+	return 'Страница обратной связи!';
+});
+
+$route->respond('GET', '/login/?', function () use ($templater, $config) {
+	return 'Страница авторизации!';
+});
+
+$route->dispatch();
+
+// video 24:41
