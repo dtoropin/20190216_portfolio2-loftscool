a5e84387840366a7d79ea8b4babb5b54c3cecc09
Denis Toropin <denis.toropin@yandex.ru>
2019-03-08 11:15:44 +0300

    add compsoer, twig, klein

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 2014
+ * Date: 06.03.2019
+ * Time: 0:06
+ */
---
+++ php/classes/Templater.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 2014
+ * Date: 06.03.2019
+ * Time: 0:32
+ */
+
+class Templater
+{
+
+}
