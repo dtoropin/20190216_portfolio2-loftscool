86baae13b1f839b1b7bcdfe574e1535b1b62c107
Denis Toropin <denis.toropin@yandex.ru>
2019-03-10 06:53:09 +0300

    final project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 20190216_portfolio2-loftscool
 Portfolio loftscool for advanced
+
+Финальная сборка в папку public/ - gulp public
--- gulpfile.js
+++ gulpfile.js
@@ -19,33 +19,33 @@ var gulp = require('gulp'),
 
 var path = {
 	build: {
-			html: 'build/',
-			js: 'build/js/',
-			css: 'build/css/',
-			img: 'build/img/',
-			fonts: 'build/fonts/'
+		html: 'build/',
+		js: 'build/js/',
+		css: 'build/css/',
+		img: 'build/img/',
+		fonts: 'build/fonts/'
 	},
 	src: {
-			html: 'app/*.html',
-			js: 'app/js/[^main]*.js',
-			jsvendor: 'app/js/main.js',
-			style: 'app/sass/*.sass',
-			img: 'app/img/**/*.*',
-			fonts: 'app/fonts/**/*.*'
+		html: 'app/*.html',
+		js: 'app/js/[^main]*.js',
+		jsvendor: 'app/js/main.js',
+		style: 'app/sass/*.sass',
+		img: 'app/img/**/*.*',
+		fonts: 'app/fonts/**/*.*'
 	},
 	watch: {
-			html: 'app/**/*.html',
-			js: 'app/js/**/*.js',
-			style: 'app/sass/**/*.sass',
-			img: 'app/img/**/*.*',
-			fonts: 'app/fonts/**/*.*'
+		html: 'app/**/*.html',
+		js: 'app/js/**/*.js',
+		style: 'app/sass/**/*.sass',
+		img: 'app/img/**/*.*',
+		fonts: 'app/fonts/**/*.*'
 	},
 	clean: './build'
 };
 
 var config = {
 	server: {
-			baseDir: "./build"
+		baseDir: "./build"
 	},
 	// tunnel: true,
 	host: 'localhost',
@@ -95,8 +95,8 @@ gulp.task('jsvendor:build', function () {
 gulp.task('style:build', function () {
 	return gulp.src(path.src.style)
 		.pipe(sourcemaps.init())
-		.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-		.pipe(rename({ suffix: '.min', prefix: '' }))
+		.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+		.pipe(rename({suffix: '.min', prefix: ''}))
 		.pipe(prefixer())
 		.pipe(cssmin())
 		.pipe(sourcemaps.write('.'))
@@ -108,17 +108,17 @@ gulp.task('style:build', function () {
 gulp.task('image:build', function () {
 	return gulp.src(path.src.img)
 		.pipe(imagemin({
-				progressive: true,
-				svgoPlugins: [{removeViewBox: false}],
-				use: [pngquant()],
-				interlaced: true
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()],
+			interlaced: true
 		}))
 		.pipe(gulp.dest(path.build.img)) //И бросим в build
 		.pipe(reload({stream: true}));
 });
 
 // fonts
-gulp.task('fonts:build', function() {
+gulp.task('fonts:build', function () {
 	return gulp.src(path.src.fonts)
 		.pipe(gulp.dest(path.build.fonts))
 });
@@ -127,7 +127,7 @@ gulp.task('fonts:build', function() {
 gulp.task('build', gulp.parallel('html:build', 'jsvendor:build', 'js:build', 'style:build', 'image:build', 'fonts:build'));
 
 // watch
-gulp.task('watch', function(){
+gulp.task('watch', function () {
 	watch([path.watch.html], gulp.series('html:build'));
 	watch([path.watch.style], gulp.series('style:build'));
 	watch([path.watch.js], gulp.series('js:build'));
@@ -147,3 +147,58 @@ gulp.task('clean', function (cb) {
 
 // default task
 gulp.task('default', gulp.series('clean', 'build', gulp.parallel('webserver', 'watch')));
+
+//////////////////////////////////////////////////////////////////////
+// final build project
+// public
+gulp.task('public:css', function () {
+	return gulp.src('build/css/*.*').pipe(gulp.dest('public/build/css/'))
+});
+gulp.task('public:js', function () {
+	return gulp.src('build/js/*.*').pipe(gulp.dest('public/build/js/'))
+});
+gulp.task('public:fonts', function () {
+	return gulp.src('build/fonts/*.*').pipe(gulp.dest('public/build/fonts/'))
+});
+gulp.task('public:img', function () {
+	return gulp.src('build/img/*.*').pipe(gulp.dest('public/build/img/'))
+});
+gulp.task('public:php', function () {
+	return gulp.src('php/*.*').pipe(gulp.dest('public/php/'))
+});
+gulp.task('public:vendor', function () {
+	return gulp.src('vendor/*.*').pipe(gulp.dest('public/vendor/'))
+});
+gulp.task('public:views', function () {
+	return gulp.src('views/*.*').pipe(gulp.dest('public/views/'))
+});
+gulp.task('public:database', function () {
+	return gulp.src('database/*.*').pipe(gulp.dest('public/database/'))
+});
+gulp.task('public:config', function () {
+	return gulp.src('config/*.*').pipe(gulp.dest('public/config/'))
+});
+gulp.task('public:index', function () {
+	return gulp.src('index.php').pipe(gulp.dest('public/'))
+});
+gulp.task('public:htaccess', function () {
+	return gulp.src('.htaccess').pipe(gulp.dest('public/'))
+});
+gulp.task('public:readme', function () {
+	return gulp.src('readme.md').pipe(gulp.dest('public/'))
+});
+
+gulp.task('public', gulp.series(
+	'public:css',
+	'public:js',
+	'public:fonts',
+	'public:img',
+	'public:php',
+	'public:vendor',
+	'public:views',
+	'public:database',
+	'public:config',
+	'public:index',
+	'public:htaccess',
+	'public:readme'
+));
